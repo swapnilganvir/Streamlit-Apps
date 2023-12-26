f97cf89c4bf0c52a2912c558a6991170d1a9fe20
swapnilganvir <75731667+swapnilganvir@users.noreply.github.com>
2023-12-26 17:19:03 +0530

    update files

--- Cars_Price_Predicter/car_price_app.py
+++ Cars_Price_Predicter/car_price_app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import pickle
 
-cars_df = pd.read_csv("./cars24-car-price.csv")
+cars_df = pd.read_csv("./car-price.csv")
 
 st.write(
     """
@@ -17,13 +17,13 @@ encode_dict = {
     "transmission_type": {"Manual":1, "Automatic":2}
 }
 
-def model_pred(fuel_type, transmission_type, engine, seats):
+def model_pred(fuel_type, transmission_type, engine, seller_type):
 
     # loading the model
-    with open("car_pred", "rb") as file:
+    with open('car_pred.pkl', mode = 'rb') as file:
         reg_model = pickle.load(file)
 
-    input_features = [[2018.0, 1, 40000, fuel_type, transmission_type, 19.70, engine, 86.3, seats]]
+    input_features = [[2018.0, seller_type, 40000, fuel_type, transmission_type, 19.70, engine, 86.3]]
     return reg_model.predict(input_features)
 
 # Layout
@@ -33,13 +33,14 @@ fuel_type = col1.selectbox("Select fuel type", ["Diesel", "Petrol", "CNG", "LPG"
 engine = col1.slider("Set the engine power", 500, 5000, step=100)
 
 transmission_type = col2.selectbox("Select transmission type", ["Manual", "Automatic"])
-seats = col2.selectbox("No. of Seats", [2,4,5,6,7])
+seller_type = col2.selectbox("Select seller type", ["Dealer", "Individual", "Trustmark Dealer"])
 
 if(st.button("Predict Price")):
     fuel_type = encode_dict['fuel_type'][fuel_type]
+    seller_type = encode_dict['seller_type'][seller_type]
     transmission_type = encode_dict['transmission_type'][transmission_type]
 
-    price = model_pred(fuel_type, transmission_type, engine, seats)
+    price = model_pred(fuel_type, transmission_type, engine, seller_type)
     st.text(f"Predicted price of the car: {price}")
 
 
